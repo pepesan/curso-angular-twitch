196d9fd0ba09740267311527ffefc7ae950eeaaf
David Vaquero <pepesan@gmail.com>
2023-04-09 17:54:47 +0200

    Creado componente Todos y paso de variables a la vista e impresión de esas variables en la vista

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+
+  public cadena: string = "Valor inicial";
+  public todo: any = {
+    text: "Valor"
+  }
+}
