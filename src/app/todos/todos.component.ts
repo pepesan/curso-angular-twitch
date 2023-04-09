581a3b06e877a193006972297a10a15d6a5f2f97
David Vaquero <pepesan@gmail.com>
2023-04-09 18:33:30 +0200

    definir y mostrar un listado por pantalla

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
 
   public cadena: string = "Valor inicial";
   public todo: any = {
     text: "Valor"
   }
+  // listado de datos a presentar
+  public todos: any[] = [];
+
+  ngOnInit(): void {
+    this.todos = [
+      {text: "Valor1"},
+      {text: "Valor2"},
+    ]
+  }
+
 }
